dd2a562c9da2dd3a591f03cb25a7ef9ba700a049
purvawebdev <purvapalankar123@gmail.com>
2025-11-28 22:47:15 +0530

    test

--- src/components/PlacementComponents/TrainingOverview.jsx
+++ src/components/PlacementComponents/TrainingOverview.jsx
@@ -100,7 +100,7 @@ export default function TrainingOverview() {
                 key={index}
                 className={`${
                   index % 2 === 0
-                    ? "bg-primary text-white"
+                    ? "bg-secondary text-white"
                     : "bg-white text-gray-900 border border-gray-200"
                 } flex flex-col items-center justify-center py-6 sm:py-8 lg:py-10 px-4 sm:px-6 rounded-md shadow-md`}
               >
